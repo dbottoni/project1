a42ed2ee99b9d99abdd77f6eb6ea7afeda028567
kmre <karla.romero7@gmail.com>
2021-07-03 17:45:55 -0400

    weather api gets info to search for weather

--- assets/js/js.js
+++ assets/js/js.js
@@ -93,6 +93,8 @@ searchBtn.addEventListener('click', eventAPI); //Made the fetch into its own fn
    
     var deleteContainer = document.getElementById("results");
         deleteContainer.remove(); // removes the container with the previous results id=results
+    var deleteContainer = document.getElementById("results-w-event");
+        deleteContainer.remove(); // removes the container with the previous results id=results
 
     console.log($(inputValue).val()); //gets input value from input text box added the script in html for jquery
     console.log(optionSelected)
@@ -105,16 +107,18 @@ searchBtn.addEventListener('click', eventAPI); //Made the fetch into its own fn
           console.log(data)
           var embedded = data['_embedded'];
           console.log(embedded)
+          var mainContainerEvents = document.getElementById("results-main");
+          var subContainerCreate = document.createElement("div");
+          subContainerCreate.setAttribute("id", "results");
+          mainContainerEvents.appendChild(subContainerCreate);
+
+          
           embedded.events.forEach((value, index, array) => {
-            console.log(index);
+            console.log("event index: " + index);
             var eventIndexed = embedded.events[index];
             var eventName = eventIndexed.name;
 
            //creates a container for each result
-            var mainContainerEvents = document.getElementById("results-main");
-            var subContainerCreate = document.createElement("div");
-            subContainerCreate.setAttribute("id", "results");
-            mainContainerEvents.appendChild(subContainerCreate);
             var containerEvents = document.getElementById("results");
             var resultContainerCreate = document.createElement("p");
             resultContainerCreate.setAttribute("id", "event-results" + index);
@@ -122,6 +126,12 @@ searchBtn.addEventListener('click', eventAPI); //Made the fetch into its own fn
             var nameTxt = "Event: " + (index + 1) + " "+ eventName + "</br>" + "URL: " + `${eventIndexed.url}`;
             resultContainerCreate.innerHTML = nameTxt; 
 
+            var lat = eventIndexed[["_embedded"]].venues[0].location.latitude;
+            var lon = eventIndexed[["_embedded"]].venues[0].location.longitude;
+            //eventIndexed._embedded.venues[0].location.latitute
+            debugger;
+            var eventIndex = index;
+            weatherAPI(lat,lon,eventIndex);
           })
 
         })
@@ -129,64 +139,85 @@ searchBtn.addEventListener('click', eventAPI); //Made the fetch into its own fn
 
   searchBtnW.addEventListener('click', getLocation); //Made the fetch into its own fn
 
-  function weatherAPI(lat, lon) {
+  function weatherAPI(latitude, longitude, eventIndex) {
     var apiKey = "d7c30de99a3b40fb84ca75fd821b8b25";
     var urlWeatherApi = "https://api.weatherbit.io/v2.0/forecast/daily?&key=";
     var zipCode = "";//$(inputValueW).val(); //zipcode has to be a number... no "" needed
 
+    var lat = latitude;
+    var lon = longitude;
+    var eventIndexW = eventIndex;
+    console.log("event index in weather: " + eventIndexW)
     //var city = ""
-    var lat;
-    var lon;
-    var daysDisplay = [3];
+
+    var daysDisplay = [3]; //max # of days to display
     var units = "I";
 
-    url = urlWeatherApi + apiKey + "&lat=" + lat + "&lon=" + lon + 
+    var url = urlWeatherApi + apiKey + "&lat=" + lat + "&lon=" + lon + 
     "&days=" + daysDisplay + "&postal_code=" + zipCode + "&units=" + units;
-    console.log(url)
-    //var maxResults = "3";
-    
-    var deleteContainer = document.getElementById("results-w");
-        deleteContainer.remove(); // removes the container with the previous results id=results
 
-    //console.log($(inputValueW).val()); //gets input value from input text box added the script in html for jquery
+    var mainContainerW = document.getElementById("results-main-w");
+    var eventContainer = document.createElement("div");
+    $(eventContainer).attr({"id": "results-w-event" + eventIndexW, "class": "temp-div-box"});
+    mainContainerW.appendChild(eventContainer);
 
       fetch (url)
-         
         .then(response => response.json())
 
         .then(data => {
-         console.log(data)
+         console.log(data.city_name)
           var dataArray = data['data'];
           var weatherCity = data.city_name;
-
+          
           dataArray.forEach((value, index, array) => {
-             console.log(index);
+            //main api variables by index
              var weatherIndexed = dataArray[index];
              var weathertemp = weatherIndexed.temp;
              var tempIcon = weatherIndexed.weather.icon;
-             var icon = "https://www.weatherbit.io/static/img/icons/" + tempIcon+ ".png"
-            console.log(icon)
+             var icon = "https://www.weatherbit.io/static/img/icons/" + tempIcon + ".png"
+             var altImg = weatherIndexed.weather.description;
+            console.log("weather index " + index)
+            console.log("city weather " + data.city_name)
             //creates a container for each result
             //looks for div with id results-main-w to append childs
-             var mainContainerW = document.getElementById("results-main-w");
-            //creates div for each day (currently set at 3) adds attribute id and appends
-             var subContainerCreateW = document.createElement("div");
-             subContainerCreateW.setAttribute("id", "results-w" + index);
-             mainContainerW.appendChild(subContainerCreateW);
-             //creates <h3> to show text for current location and appends days from API
-             var containerW = document.getElementById("results-w" + index);
-             var hContainer = document.createElement("h3");
-             containerW.appendChild(hContainer);
-             hContainer.innerHTML = "Weather for current location Day " + (index + 1) + ": " + `${weatherIndexed.datetime}` + "</br>";
-             //creates <p> with id to append the results from the API
-             var resultContainerCreateW = document.createElement("p");
-             resultContainerCreateW.setAttribute("id", "event-results-w" + index);
-             containerW.appendChild(resultContainerCreateW);
-             var nameTxt = "Temp "  + ": "+ weathertemp + " F" + " " + "</br>" + "City: " + `${weatherCity}` + 
-             " ";
-             resultContainerCreateW.innerHTML = nameTxt; 
-          })
 
+              var mainContainerW = document.getElementById("results-w-event" + eventIndexW);
+              console.log("weather event index " + eventIndexW)
+               //creates div for each day (currently set at 3) adds attribute id and appends
+               var subContainerCreateW = document.createElement("div");
+               $(subContainerCreateW).attr({"id": "results-w-day" + index, "class": "temp-div-box"});
+               mainContainerW.appendChild(subContainerCreateW);
+               
+               //creates <h3> to show text for current location and appends days from API
+               //var containerW = document.getElementById("results-w" + eventIndexW);
+               var hContainer = document.createElement("h3");
+               subContainerCreateW.appendChild(hContainer);
+               hContainer.innerHTML = "Weather for current location Day " + (index + 1) + ": " + `${weatherIndexed.datetime}` + "</br>";
+               
+               //creates <p> with id to append the results from the API
+               var pW = document.createElement("p");
+               //$(pW).attr({"id": "event-results-w" + index});
+               subContainerCreateW.appendChild(pW);
+               var nameTxt = "Temp "  + ": "+ weathertemp + " F" + " " + "</br>" + "City: " + `${weatherCity}` + 
+               " ";
+               pW.innerHTML = nameTxt; 
+  
+               //creates div for each image/day (currently set at 3) adds attribute id and appends
+               //var imgContainerW = document.getElementById("results2-w" + index);
+              //  var imgCreateW = document.createElement("div");
+              //  $(imgCreateW).attr({"id": "img-w" + index, "class": "div-icon"});
+              //  subContainerCreateW.appendChild(imgCreateW);
+              
+               //creates <img> to show current weather icon
+              //  var imgW = document.getElementById("img-w" + eventIndexW);
+               var imgContainer = document.createElement("img");
+               $(imgContainer).attr({"src":icon, "class":"icon", "alt":altImg});
+               subContainerCreateW.appendChild(imgContainer);
+               console.log(index)
+               console.log(icon)
+              
+
+          })
         })
   };
 
